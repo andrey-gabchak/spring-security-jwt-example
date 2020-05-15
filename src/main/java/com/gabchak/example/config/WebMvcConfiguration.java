8d2a0c15b41d6fa69f769e48bd1624553ac5555f
Gabchak <andrey.gabchak@gmail.com>
2020-05-15 22:00:49 +0300

    fix import

--- src/main/java/com/gabchak/example/config/WebMvcConfiguration.java
+++ src/main/java/com/gabchak/example/config/WebMvcConfiguration.java
@@ -3,7 +3,7 @@ package com.gabchak.example.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.reactive.config.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
